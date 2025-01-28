798ff6c2cf6041feda509ed16ba6ab8e3ad0a734
zeeshk <zeeshan.sask@gmail.com>
2025-01-29 01:19:05 +0530

    bd1 assignment excercise 1, 2, 3

--- index.js
+++ index.js
@@ -19,7 +19,35 @@ app.get('/cart-total', (req, res) => {
   let newItemPrice = parseFloat(req.query.newItemPrice);
   let cartTotal = parseFloat(req.query.cartTotal);
   let result = calculateCartTotal(newItemPrice, cartTotal);
-  return res.json(result);
+  return res.send(result.toString());
+});
+
+function calculateDiscount(discount, cartTotal){
+  let discountprice = (discount/100)*cartTotal;
+  let finalprice = cartTotal-discountprice;
+  return finalprice;
+}
+app.get('/membership-discount',(req,res)=>{
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let isMember = req.query.isMember === "true";
+  let discount = 10;
+  if(isMember){
+    let result = calculateDiscount(discount, cartTotal);
+    res.send(result.toString());
+  }else{
+    return res.send("You are not logged in");
+  }
+});
+
+function calculateTax(tax, cartTotal){
+  let taxprice = (tax/100)*cartTotal;
+  return taxprice;
+}
+app.get('/calculate-tax',(req,res)=>{
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let tax = 5;
+    let result = calculateTax(tax, cartTotal);
+    res.send(result.toString());
 });
 
 app.listen(port, () => {
