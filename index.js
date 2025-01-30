dc0d366b7226d922f533e840abcc17e550461e8e
zeeshk <zeeshan.sask@gmail.com>
2025-01-30 22:37:18 +0530

    bd1 exercises 4,5,6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { rmSync } = require('fs');
 const { resolve } = require('path');
 
 const app = express();
@@ -10,6 +11,7 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+//Exercise 1
 function calculateCartTotal(newItemPrice, cartTotal) {
   let total = newItemPrice + cartTotal;
   return total;
@@ -22,6 +24,7 @@ app.get('/cart-total', (req, res) => {
   return res.send(result.toString());
 });
 
+//Exercise 2
 function calculateDiscount(discount, cartTotal){
   let discountprice = (discount/100)*cartTotal;
   let finalprice = cartTotal-discountprice;
@@ -39,6 +42,7 @@ app.get('/membership-discount',(req,res)=>{
   }
 });
 
+//Exercise 3
 function calculateTax(tax, cartTotal){
   let taxprice = (tax/100)*cartTotal;
   return taxprice;
@@ -48,6 +52,46 @@ app.get('/calculate-tax',(req,res)=>{
   let tax = 5;
     let result = calculateTax(tax, cartTotal);
     res.send(result.toString());
+});
+
+//Exercise 4
+function calculateDeliveryTime(distance, shippingMethod){
+  let time = '';
+  if(shippingMethod == 'Standard'){
+    time = distance/50;
+  }else if(shippingMethod == 'Express'){
+    time = distance/100
+  }
+  return time;
+}
+app.get('/estimate-delivery', (req,res)=>{
+  let shippingMethod = req.query.shippingMethod;
+  let distance = parseFloat(req.query.distanceinkms);
+  let result = calculateDeliveryTime(distance, shippingMethod);
+  res.send(result.toString());
+});
+
+//Exercise 5
+function calculateShippingCost(weight,distance){
+  let cost = weight*distance*0.1;
+  return cost;
+}
+app.get('/shipping-cost',(req,res)=>{
+  let weight = parseFloat(req.query.weight);
+  let distance = parseFloat(req.query.distance);
+  let result = calculateShippingCost(weight, distance);
+  return res.send(result.toString());
+});
+
+//Exercise 6
+function calculateLoyaltyPoints(purchaseAmount){
+  let loyaltypoints = purchaseAmount*2;
+  return loyaltypoints;
+}
+app.get('/loyalty-points',(req,res)=>{
+  let purchaseAmount = parseFloat(req.query.purchaseAmount);
+  let result = calculateLoyaltyPoints(purchaseAmount);
+  return res.send(result.toString());
 });
 
 app.listen(port, () => {
